a9d3770e327c9f4652a2f09577de13e7611886a3
Serg Gini <kornburn@gmail.com>
2023-05-21 17:37:31 +0300

    Initial commit
    Simple version for R based on python code from GeoHot (tinygrad)

---
+++ gemm.R
@@ -0,0 +1,21 @@
+N <- 2048
+
+zz <- file("/tmp/matmul", "rb")
+
+A <- matrix(readBin(zz, "double", N*N, size = 4), nrow = N, ncol = N, byrow = TRUE)
+B <- matrix(readBin(zz, "double", N*N, size = 4), nrow = N, ncol = N, byrow = TRUE)
+C <- matrix(readBin(zz, "double", N*N, size = 4), nrow = N, ncol = N, byrow = TRUE)
+
+close(zz)
+gflop <- 2.0*N*N*N*1e-9
+
+Bt = t(B)
+for(i in 1:4) {
+  start <- Sys.time()
+  check <- A %*% B
+  #check <- tcrossprod(A, Bt)
+  end <- Sys.time()
+  s = (end - start)
+  out <- paste0(format(gflop/as.numeric(s), digits = 3)," GFLOP/S -- ", format(as.numeric(s)*1e3, digits = 3), " ms\n")
+  print(out)
+}
